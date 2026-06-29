c546138f9155a5424d65db1ebd8bb292b032ae72
cesaraasouza <cesar.aasouza@gmail.com>
2026-06-29 09:28:04 -0300

    Add files via upload
    3rd version - improvements

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+{
+  "name": "Flip 7 Score",
+  "short_name": "Flip7",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#070914",
+  "theme_color": "#090b18",
+  "icons": [
+    {"src":"icons/icon-192.png","sizes":"192x192","type":"image/png"},
+    {"src":"icons/icon-512.png","sizes":"512x512","type":"image/png"}
+  ]
+}
